77edc89a4cda41ec520163749fa499da4ba42b81
Charles Bell <cbell@MacBook-Pro.local>
2019-02-25 19:41:44 -0500

    Updates for Lesson 5

---
+++ source/icehouse1.py
@@ -0,0 +1,9 @@
+# icehouse1.py Ð Building an ice house
+from mcpi.minecraft import Minecraft 
+from mcpi import block
+mc = Minecraft.create()
+p = mc.player.getTilePos()
+mc.setBlocks(p.x + 1, p.y, p.z + 1,
+             p.x + 10, p.y + 5, p.z + 10, block.ICE)
+mc.setBlocks(p.x + 2, p.y + 1, p.z + 2, 
+             p.x + 9, p.y + 4, p.z + 9, block.AIR)
---
+++ source/icehouse2.py
@@ -0,0 +1,14 @@
+# icehouse2.py Ð Building an ice house with carpet
+from mcpi.minecraft import Minecraft 
+from mcpi import block
+mc = Minecraft.create()
+p = mc.player.getTilePos()
+mc.setBlocks(p.x + 1, p.y, p.z + 1,
+             p.x + 10, p.y + 5, p.z + 10, block.ICE)
+mc.setBlocks(p.x + 2, p.y + 1, p.z + 2, 
+             p.x + 9, p.y + 4, p.z + 9, block.AIR)
+mc.setBlocks(p.x + 5, p.y + 1, p.z + 1, 
+             p.x + 6, p.y + 3, p.z + 1, block.AIR)
+mc.setBlocks(p.x + 2, p.y, p.z + 2, 
+             p.x + 9, p.y, p.z + 9, block.WOOL.id, 14)
+
